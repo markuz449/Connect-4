6f6739f2712c8c23d8d4d9a2498525b6df1b228e
markuz449 <markuz449@gmail.com>
2020-11-12 14:13:46 +1300

    Added logging to the server

--- Online/server.js
+++ Online/server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var errorHandler = require("errorhandler");
 var {v4: uuidv4 }  = require("uuid");
+var {transports, createLogger, format} = require('winston');
 
 var app = express();
 var root = __dirname + "/public";
@@ -20,11 +21,17 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-// Simple logger
-/*app.use(function(req, res, next){
-  console.log("%s %s", req.method, req.url);
-  next();
-});*/
+// Simple Winston logger
+const logger = createLogger({
+  format: format.combine(
+      format.timestamp(),
+      format.json()
+  ),
+  transports: [
+      new transports.Console(),
+      new transports.File({filename: 'logs/Connect-4.log', level:'info'})
+  ]
+});
 
 // Error handler
 app.use(errorHandler({
@@ -45,16 +52,16 @@ const io = require("socket.io")(server);
 
 // Listen on every connection
 io.on('connection', (socket) => {
-  console.log('New player connected: ' + socket.id);
+  logger.info({message:('New player connected: ' + socket.id)});
   var new_player = {user_id: socket.id, game_id: null};
   current_players.push(new_player);
-  console.log("Current Players: " + current_players.length);
+  logger.info({message:("Current Players: " + current_players.length)});
   socket.emit('new_player', {user_id: socket.id, online_num: current_players.length});
 
   // Listens if a player disconnects
   // Checks if they were in a game or not, if so, tell the opponent that they forfeited
   socket.on('disconnect', () => {
-    console.log("Player: " + socket.id + " disconnecting...");
+    logger.info({message:("Player: " + socket.id + " disconnecting...")});
     var remove_index = get_player_index(socket.id);
 
     if (remove_index > -1) {
@@ -63,7 +70,7 @@ io.on('connection', (socket) => {
       // Remove player from game and sends opponent disconnect
       if (disconnected_player.game_id != null){
         let game_index = get_game_index(disconnected_player.game_id);
-        console.log("Disconnecting from game: " + current_games[game_index].game_id);
+        logger.info({message:("Disconnecting from game: " + current_games[game_index].game_id)});
         if(current_games[game_index].player1 == disconnected_player.user_id){
           io.to(current_games[game_index].player2).emit('opponent_disconnect');
           current_games[game_index].player1 = null;
@@ -73,13 +80,13 @@ io.on('connection', (socket) => {
           current_games[game_index].player2 = null;
         }
         else {
-          console.log("What the fuck happened...");
+          logger.error({message:("Error in removing player from game while disconnecting")});
         }
 
         // Checks if the game needs to be removed
         if (current_games[game_index].player1 == null && current_games[game_index].player2 == null){
           current_games.splice(game_index, 1);
-          console.log("Removed a game via disconnect, current list: " + current_games);
+          logger.info({message:("Removed a game via disconnect, current list: " + current_games)});
         }
       }
 
@@ -99,13 +106,13 @@ io.on('connection', (socket) => {
         io.to(current_game.player1).emit('opponents_move', data);
       }
     } else{
-      console.log("Error with game communication");
+      logger.info({message:("Error with game communication")});
     }
   });
 
   // Listens if the user wants to play against a new opponent
   socket.on('new_opponent', (data) => {
-    console.log("New Opponent: User ID: " + data.user_id);
+    logger.info({message:("New Opponent: User ID: " + data.user_id)});
     var player_index = get_player_index(data.user_id);
     current_players[player_index].game_id = null;
 
@@ -124,12 +131,13 @@ io.on('connection', (socket) => {
     // If both players have left, remove the game from the list
     if (game.player1 == null && game.player2 == null){
       current_games.splice(game_index, 1);
-      console.log("Removed a game via play_again(), current list: " + current_games);
+      logger.info({message:("Removed a game via play_again(), current list: " + current_games)});
     }
   });
 
   // Listens if the player wants a rematch
   socket.on('rematch', (data) => {
+    logger.info({message:("Rematch request: User ID: " + data.user_id)});
     var game_index = get_game_index(data.game_id);
     if (game_index >= 0){
       current_game = current_games[game_index];
@@ -143,7 +151,7 @@ io.on('connection', (socket) => {
   
   // Listens if the opponent doesn't make a move in time
   socket.on('timeout', (data) => {
-    console.log("Timeout from: " + data.user_id);
+    logger.info({message:("Timeout from: " + data.user_id)});
     var game_index = get_game_index(data.game_id);
     if (game_index >= 0){
       let timeout_game = current_games[game_index];
@@ -170,7 +178,7 @@ function start_game(){
     }
     if (avaliable_players.length > 1){
       var game_id = uuidv4();
-      console.log("Starting New Game: " + game_id);
+      logger.info({message:("Starting New Game: " + game_id)});
       var player1_id = avaliable_players[0].user_id;
       var player2_id = avaliable_players[1].user_id;
       var player_start = Math.round(Math.random());
@@ -213,7 +221,7 @@ function get_game_index(game_id){
     }
   }
   if (game_index == -1){
-    console.log("Error in getting game-index");
+    logger.error({message:("Error in getting game-index")});
   }
   return game_index;
 }
@@ -227,7 +235,7 @@ function get_player_index(user_id){
     }
   }
   if (player_index == -1){
-    console.log("Error in getting player-index");
+    logger.error({message:("Error in getting player-index")});
   }
   return player_index;
 }
